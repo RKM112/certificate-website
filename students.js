a10e2e82d28952cdaeb55b40374a3b236ccdb955
RKM112 <130380610+RKM112@users.noreply.github.com>
2025-06-23 12:43:27 +0530

    Add files via upload

---
+++ students.js
@@ -0,0 +1,17 @@
+
+const students = [
+  {
+    mobile: "9876543210",
+    name: "Rahul Kumar",
+    workshop: "Professional Audio Workshop",
+    location: "Indore, Madhya Pradesh",
+    date: "3rd March to 10th March 2025"
+  },
+  {
+    mobile: "9123456789",
+    name: "Priya Sharma",
+    workshop: "Sound Engineering Basics",
+    location: "Bhopal, MP",
+    date: "5th April to 7th April 2025"
+  }
+];
